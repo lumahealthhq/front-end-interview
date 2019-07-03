7b1087824d54ebf1adbbe0bc506f01b3b6d53a27
David Arellano <dtarellano1@gmail.com>
2019-07-02 22:53:48 -0700

    breakdown componet for better readability

--- src/components/Success.js
+++ src/components/Success.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Success = (props) => <div id="Success">Success! You have Submitted { props.count } referrals. You will be notified once they've been approved</div>
+const Success = (props) => {
+  const count = props.count
+  return (
+    <div id="Success">
+      Success! You have Submitted { count } referrals. You will be notified once they've been approved
+    </div>
+  )
+}
 
 export default Success
