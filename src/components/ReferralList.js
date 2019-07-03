154bce48113ffe205f2de298ced73f96ebd3a725
David Arellano <dtarellano1@gmail.com>
2019-07-02 23:03:51 -0700

    removed unneeded if statement

--- src/components/ReferralList.js
+++ src/components/ReferralList.js
@@ -3,22 +3,18 @@ import ListItem from './ListItem.js';
 
 const ReferalList = (props) => {
   const listLength = props.list.length;
-  let list = ''
-  if (listLength) {
+  let list = '';
 
-    list = props.list.map((data, index) => {
-      let expanded = true;
-      let title = 'New Referral';
-      if (listLength > 1 && index < listLength - 1) {
-        expanded = false;
-        title = `${data.firstName} ${data.lastName}`
-      }
-      return <ListItem title={title} formData={data} key={index} expanded={expanded} index={index} handleChange={props.handleChange} handleDelete={props.handleDelete}/>
-    })
-  } else {
-    list = null
-  }
-  return(
+  list = props.list.map((data, index) => {
+    let expanded = true;
+    let title = 'New Referral';
+    if (listLength > 1 && index < listLength - 1) {
+      expanded = false;
+      title = `${data.firstName} ${data.lastName}`
+    }
+    return <ListItem title={title} formData={data} key={index} expanded={expanded} index={index} handleChange={props.handleChange} handleDelete={props.handleDelete}/>
+  })
+  return (
     <React.Fragment>
       { list }
     </React.Fragment>
