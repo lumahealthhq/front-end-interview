511c75feb3702ffbf6d27a72ee07988924ea7ab0
Marco Modena <marco.modena.serv@gmail.com>
2020-08-30 19:11:21 -0300

    added basic components

---
+++ src/Components/ReferralForm/ReferralForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReferralCard from '../ReferralCard/ReferralCard';
+import './ReferralForm.css';
+
+const ReferralForm = () => {
+  return (
+    <div>
+      <ReferralCard />
+    </div>
+  );
+}
+
+export default ReferralForm;
