b4931d396b49c3d1182f7221f7a249b1fd956015
Abenezer-Tilahun <abenezermesafint2128@gmail.com>
2022-08-20 11:13:58 +0300

    Add a basic componets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReferralForm from './Components/Form/ReferralForm';
+import ReferralHeader from './Components/Header/ReferralHeader';
 import './App.css';
 
-function App() {
+const App= () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div >
+      <ReferralForm/>
+      <ReferralHeader/>
     </div>
   );
 }
